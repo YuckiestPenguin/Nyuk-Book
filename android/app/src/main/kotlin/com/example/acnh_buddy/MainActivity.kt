5e187831907ce3c980c0c2c42aec868f1db78691
Vico Melo <vinicomelo@gmail.com>
2020-06-01 14:57:37 -0400

    added splash screen

--- android/app/src/main/kotlin/com/example/acnh_buddy/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/acnh_buddy/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.acnh_buddy
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
