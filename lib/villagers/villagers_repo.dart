a4cec909a01edfab258bad36fb816ea4e5664c2d
Vico Melo <vinicomelo@gmail.com>
2020-06-01 11:23:26 -0400

    villagers repo

---
+++ lib/villagers/villagers_repo.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:acnh_buddy/global/global_endpoints.dart';
+import 'package:http/http.dart' as http;
+
+class VillagersRepo {
+  Future getAllVillagers() async {
+    final url = '${GlobalEndpoints().baseUrl}fish';
+
+    try {
+      var response = await http.get(url);
+
+      List villagers = [];
+
+      json.decode(response.body).forEach((key, value) => villagers.add(value));
+      villagers.sort((a, b) {
+        return a['file-name']
+            .toString()
+            .toLowerCase()
+            .compareTo(b['file-name'].toString().toLowerCase());
+      });
+      return villagers;
+    } catch (error) {
+      throw (error);
+    }
+  }
+}
